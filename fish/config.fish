ffb767d62473b00b801199d8a4e5d97e951a7eea
VerticalHeretic <ls.warss98@gmail.com>
2025-09-26 16:04:12 +0200

    First part of the cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -3,13 +3,13 @@ if status is-interactive
 end
 
 starship init fish | source
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/verticalheretic/.lmstudio/bin
-
 pyenv init - fish | source
 
-# Added by `rbenv init` on Wed Aug  6 15:56:27 CEST 2025
-status --is-interactive; and rbenv init - --no-rehash fish | source
-/Users/verticalheretic/.local/bin/mise activate fish | source
-~/.local/bin/mise activate fish | source
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
